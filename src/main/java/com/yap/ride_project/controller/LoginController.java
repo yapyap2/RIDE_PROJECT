b473716c75d6d6b3871caf193ea42b9e88f0b095
yapyap2 <wonwoo42@gmail.com>
2024-04-18 10:36:54 +0900

    LoginController @CrossOrigin add

--- src/main/java/com/yap/ride_project/controller/LoginController.java
+++ src/main/java/com/yap/ride_project/controller/LoginController.java
@@ -5,12 +5,14 @@ import com.yap.ride_project.dto.SignInRequestDTO;
 import com.yap.ride_project.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class LoginController {
 
     private final LoginService loginService;
